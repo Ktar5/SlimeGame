65c35b93c67edce02f81eacf1a19a5a172ef7b4c
Carter Gale <ktarfive@gmail.com>
2019-06-08 22:28:26 -0700

    More levels from friend

--- core/src/main/java/com/ktar5/slime/analytics/LevelSession.java
+++ core/src/main/java/com/ktar5/slime/analytics/LevelSession.java
@@ -1,11 +1,33 @@
 package com.ktar5.slime.analytics;
 
+import com.ktar5.gameengine.util.Side;
+
 import java.util.UUID;
 
 public class LevelSession {
     public static UUID currentLevelSession = UUID.randomUUID();
 
-    public static void newSession(){
+    private static StringBuilder builder = new StringBuilder();
+    private static int currentFrame = 0;
+
+    public static void newSession() {
         currentLevelSession = UUID.randomUUID();
+        builder = new StringBuilder();
+        currentFrame = 0;
     }
+
+    public static void addInputOnFrame(Side side) {
+        builder.append(currentFrame);
+        currentFrame = 0;
+        builder.append(side.string);
+    }
+
+    public static void incrementFrame() {
+        currentFrame += 1;
+    }
+
+    public static String serializeInputs() {
+        return builder.toString();
+    }
+
 }

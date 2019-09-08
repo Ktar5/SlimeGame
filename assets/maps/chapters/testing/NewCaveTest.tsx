daf615c28c39c76ea5719a1184bbb507b9aa0db0
Carter Gale <ktarfive@gmail.com>
2019-09-07 19:04:53 -0700

    More modifications to tilesets. removed and backed up old

--- assets/maps/chapters/testing/NewCaveTest.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="NewCaveTest" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
- <image source="../../tileset/NewCaveTest.png" width="512" height="512"/>
-</tileset>
